859ec14aee135058797f18e2a4434d9c3b0a2c87
Anna Khit <annakhit02@mail.ru>
2024-11-26 19:19:08 +0300

    Implemented MyHashSet Class

--- Program.cs
+++ Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KASDLab23
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            MyHashSet<int> hashSet = new MyHashSet<int>();
+
+            hashSet.Add(1);
+            hashSet.Add(6);
+            hashSet.Add(3);
+            hashSet.Add(14);
+            hashSet.Add(17);
+            hashSet.Add(24);
+            hashSet.Add(28);
+            hashSet.Add(4);
+
+            foreach (var value in hashSet.ToArray())
+            {
+                Console.WriteLine("Value: {0}", value);
+            }
+
+            Console.WriteLine("Contains(14): {0}", hashSet.Contains(14));
+
+            Console.WriteLine("Remove(14): {0}", hashSet.Remove(14));
+
+            Console.WriteLine("Contains(14): {0}", hashSet.Contains(14));
+
+            Console.WriteLine("Size(): {0}", hashSet.Size());
+
+            Console.WriteLine("First(): {0}", hashSet.First());
+
+            Console.WriteLine("Last(): {0}", hashSet.Last());
+
+            foreach (var value in hashSet.SubSet(3, 24).ToArray())
+            {
+                Console.WriteLine("SubSet(3, 24): {0}", value);
+            }
+
+            foreach (var value in hashSet.TailSet(15).ToArray())
+            {
+                Console.WriteLine("TailSet(15): {0}", value);
+            }
+
+            foreach (var value in hashSet.HeadSet(15).ToArray())
+            {
+                Console.WriteLine("headSet(15): {0}", value);
+            }
+
+            Console.ReadKey();
         }
     }
 }
